0fd2295a74abe7bedd8cc50a7993e231dacb4000
tofuhead2015 <tofuhead@gmail.com>
2022-11-11 15:59:31 -0500

    docstring

--- app.py
+++ app.py
@@ -9,13 +9,20 @@ app.config['SECRET_KEY'] = "top secret"
 
 @app.route('/')
 def show_board(): 
-    session["board"] = boggle_game.make_board()
+    '''
+    >>> Initialize the session board
+    '''
+    session["board"] = boggle_game.make_board()    
     return render_template('base.html', board = session["board"])
     
 
 @app.route('/', methods=['POST'])
 def verify_word(): 
+    '''
+    >>> verify the word entered
+    '''
     guess = request.args.get('word')
+    print("verify_word: ", guess)    
     result = boggle_game.check_valid_word(session["board"], guess)
     return {"result": "result - " + result}
 
--- static/app.js
+++ static/app.js
@@ -1,6 +1,10 @@
 document.getElementById('submit-guess').addEventListener('click', guess_word);
+//document.getElementById('guess').addEventListener('keypress', guess_word);
+
 const idTimer = setInterval(showTime, 1000);
 let time_left = 60
+let score = 0
+let words = new Set()
 
 function showTime(){
     document.getElementById('timer').innerText = time_left;
@@ -14,9 +18,19 @@ function showTime(){
 async function guess_word(e){
     if (time_left <= 0) return;
 
+    console.log(time_left);
+
     e.preventDefault();
-    const guess = document.getElementById('guess').value
+    
+    let guess = document.getElementById('guess').value;
     console.log(guess);
+
+    if (guess.length === 0) return;
+
+    if (words.has(guess)) return;
+
+    words.add(guess);
+
     const response = await axios({
         url: `/` + "?word=" + guess,
         method: "POST",
@@ -24,5 +38,11 @@ async function guess_word(e){
     console.log(response)
     const result = response.data["result"]
     document.getElementById('guess-result').innerText = result
+    if (result === 'result - ok') {
+        score += guess.length;
+        document.querySelector('#score').innerText = "Score: " + score;
+    }
+    document.getElementById('guess').value = "";
+    document.getElementById('guess').focus();
 }
 
